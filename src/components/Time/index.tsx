a77bc3f3150a6608dddda5a616a15997b11f934b
Eduardo Araujo de Azaredo <79063074+Kleie@users.noreply.github.com>
2023-07-30 18:04:25 -0300

    fifth commit

--- src/components/Time/index.tsx
+++ src/components/Time/index.tsx
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { format, addDays, startOfWeek, startOfDay, differenceInSeconds } from "date-fns";
+import { useState, useEffect } from "react";
+import { addDays, startOfWeek, startOfDay, formatDistance } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export function TimeLeft() {
   const [tempoRestante, setTempoRestante] = useState("");
 
-  // Função para calcular o tempo restante até o próximo reset diário (00:00)
   function calcularTempoRestante(): string {
     const agora = new Date();
     const proximoReset = startOfDay(addDays(agora, 1)); // Calcula o próximo reset diário (amanhã à meia-noite)
-    const diferencaSegundos = differenceInSeconds(proximoReset, agora);
-    const duracaoFormatada = format(new Date(0, 0, 0, 0, 0, diferencaSegundos), `HH:mm 'horas restantes'`);
-    return duracaoFormatada;
+    const distancia = formatDistance(agora, proximoReset, {
+      // addSuffix: true,
+      locale: ptBR,
+    });
+    return distancia;
   }
 
-  // Atualizar o tempo restante quando o componente é montado
   useEffect(() => {
     const intervalId = setInterval(() => {
       const tempoRestanteAtualizado = calcularTempoRestante();
       setTempoRestante(tempoRestanteAtualizado);
     }, 60000);
 
-    // Limpar o intervalo quando o componente é desmontado
     return () => clearInterval(intervalId);
-  }, []); // O array de dependências vazio garante que o efeito seja executado apenas uma vez na montagem do componente
+  }, []);
 
   return <span>{tempoRestante}</span>;
 }
@@ -32,10 +32,37 @@ export function WeekLeft() {
 
   function calcularTempoRestante(): string {
     const agora = new Date();
-    const proximoSabado = startOfWeek(addDays(agora, ((6 - agora.getDay() + 7) % 7) + 1)); // Calcula o próximo sábado
-    const diferencaSegundos = differenceInSeconds(proximoSabado, agora);
-    const duracaoFormatada = format(new Date(0, 0, 0, 0, 0, diferencaSegundos), `HH:mm 'horas restantes'`);
-    return duracaoFormatada;
+    const proximaQuinta = startOfWeek(agora, { weekStartsOn: 4 }); // Calcula a próxima quinta-feira
+    const distancia = formatDistance(new Date(agora), new Date(proximaQuinta), {
+      addSuffix: true,
+      locale: ptBR,
+    });
+    return distancia;
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const tempoRestanteAtualizado = calcularTempoRestante();
+      setTempoRestante(tempoRestanteAtualizado);
+    }, 60000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return <span>{tempoRestante}</span>;
+}
+
+export function TerraDoAmanhecer() {
+  const [tempoRestante, setTempoRestante] = useState("");
+
+  function calcularTempoRestante(): string {
+    const agora = new Date();
+    const proximoSabado = startOfWeek(agora, { weekStartsOn: 6 }); // Calcula o próximo sábado
+    const distance = formatDistance(new Date(agora), new Date(proximoSabado), {
+      // addSuffix: true,
+      locale: ptBR,
+    });
+    return distance;
   }
 
   useEffect(() => {
